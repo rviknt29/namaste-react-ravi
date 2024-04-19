0ffa88518b8dcc12cacd5d691071a6bbeed63772
Ravi <rviknt29@gmail.com>
2024-04-20 01:40:17 +0530

    Lecture about component

--- App.js
+++ App.js
@@ -3,10 +3,25 @@ import ReactDOM from "react-dom/client";
 
 // React Element
 // const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+// JSX heading
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTML render
+const heading = <h1 id="heading">React meri jaan - heading element</h1>;
 
-// JSX heading 
-const jsxHeading = <h1 id="heading">Namaste React using jsx</h1>;
+// React Components
+// 1. React Functional component
 
+const Title = () => (
+  <h1 className="title">React meri jaan - Title component</h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1>React meri jaan - Functional Component</h1>
+    {heading}
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(heading);
+root.render(<HeadingComponent />);
