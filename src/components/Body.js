df82615d8b6a260410499371f4830e6269750466
Ravi <rviknt29@gmail.com>
2024-05-09 00:08:10 +0530

    fatching live data from API & display in UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurents, setListOfRestaurents] = useState(resList);
+  const [listOfRestaurents, setListOfRestaurents] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    const resListFromAPI =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setListOfRestaurents(resListFromAPI);
+  };
+
+  const handleFilterTopRatedRestaurents = () => {
+    const filteredResList = listOfRestaurents.filter(
+      (res) => res.info.avgRating > 4
+    );
+    console.log(filteredResList);
+    setListOfRestaurents(filteredResList);
+  };
 
   return (
     <div className="body">
       <div className="search">
         <button
           className="filter-btn"
-          onClick={() => {
-            const filteredResList = resList.filter((res) => res.rating > 4);
-            console.log(filteredResList);
-            setListOfRestaurents(filteredResList);
-          }}
+          onClick={handleFilterTopRatedRestaurents}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
         {listOfRestaurents.map((restaurent) => (
-          <RestaurantCard key={restaurent.id} resData={restaurent} />
+          <RestaurantCard key={restaurent.info.id} resData={restaurent.info} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,7 +3,8 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { resName, cuisine, rating, time } = resData;
+  console.log("resData --> ", resData);
+  const { name, avgRating, costForTwo, cloudinaryImageId } = resData;
 
   const styleCard = {
     backgroundColor: "#f0f0f0",
@@ -11,11 +12,16 @@ const RestaurantCard = (props) => {
 
   return (
     <div className="res-card" style={styleCard}>
-      <img alt="res-logo" className="res-logo" src={CDN_URL} />
-      <h3>{resName}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{rating} Stars</h4>
-      <h4>{time}</h4>
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3>{name}</h3>
+      <h4>{resData.cuisines.join(", ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{resData.sla.deliveryTime} Minuts</h4>
     </div>
   );
 };
