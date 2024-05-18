8f8de3a4a4e0d93b730186841514f696e537cce2
Ravi <rviknt29@gmail.com>
2024-05-19 01:44:29 +0530

    Router setting in application

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>This is Our Contact details</h1>
+      <h2>How can I help you ?</h2>
+    </div>
+  );
+};
+
+export default Contact;
